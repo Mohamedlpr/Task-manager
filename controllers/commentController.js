02328af19d93fe42e10368a97f8a7e0a7bcc4528
Mohamedlpr <m.osama.dev@gmail.com>
2026-08-11 16:05:31 +0300

    Comment creation final fix

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -5,7 +5,17 @@ exports.createComment = async (req, res) => {
     const task = req.params.taskId;
     const data = req.body.text;
     const comment = await Comment.create({ text: data, taskId: task });
-    return res.status(200).json(comment);
+    return res.status(201).json(comment);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+exports.getComments = async (req, res) => {
+  try {
+    const task = req.params.taskId;
+    const comment = await Comment.find({ taskId: task }).populate("taskId");
+    res.status(200).json(comment);
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const router = express.Router();
-const { createComment } = require("../controllers/commentController");
+const router = express.Router({ mergeParams: true });
+const { createComment, getComments } = require("../controllers/commentController");
 
 router.post("/", createComment);
+router.get("/", getComments);
 
 module.exports = router;
