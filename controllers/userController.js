b0fc57bf0285e8b37cdb9fb8eaa1579258cbac4c
Mohamedlpr <m.osama.dev@gmail.com>
2026-08-14 04:26:20 +0300

    Fix SignUp

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,18 @@
+const { json } = require("express");
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
 
 exports.createUser = async (req, res) => {
   try {
     const userEmail = req.body.email;
+    const userEmailVerification = await User.findOne({ email: userEmail });
+    if (userEmailVerification) {
+      return res.status(400).json({ message: "email already registered" });
+    };
     const userPassword = req.body.password;
     const hashedPassword = await bcrypt.hash(userPassword, 10);
     const user = await User.create({ email: userEmail, password: hashedPassword });
-    res.status(200).json({ message: "Signed Up successfully" });
+    res.status(201).json({ message: "signed up successfully" });
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
