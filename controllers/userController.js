3e1a3c056972ec1aa320cad5ab59c5e73230e32c
Mohamedlpr <m.osama.dev@gmail.com>
2026-08-14 15:34:47 +0300

    signin with encryption and JWT

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
-const { json } = require("express");
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 exports.createUser = async (req, res) => {
   try {
@@ -17,3 +18,22 @@ exports.createUser = async (req, res) => {
     res.status(400).json({ message: err.message });
   }
 };
+
+exports.signin = async (req, res) => {
+  try {
+    const userEmail = req.body.email;
+    const userPassword = req.body.password;
+    const userEmailVerification = await User.findOne({ email: userEmail });
+    if (!userEmailVerification) {
+      return res.status(400).json({ message: "user was not found" });
+    };
+    const password = await bcrypt.compare(userPassword, userEmailVerification.password);
+    if (!password) {
+      return res.status(401).json({ message: "password is not correct" })
+    }
+    const token = await jwt.sign({ id: User._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+    res.status(200).json(token)
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,8 @@
-const { createUser } = require("../controllers/userController");
+const { createUser, signin } = require("../controllers/userController");
 const express = require("express");
 const router = express.Router();
 
 router.post("/signup", createUser);
+router.post("/signin", signin);
 
 module.exports = router;
