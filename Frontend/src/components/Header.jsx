64adb2bbc03cb0ac18c1915e18371de71fb43295
Mohamedlpr <m.osama.dev@gmail.com>
2026-08-19 03:35:13 +0300

    changing logo and editing navbar

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -2,20 +2,22 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <header className="nav">
-      <img src="/favicon.png" alt="Task Manger Logo" />
-      <div className="navContainer">
-        <Link className="navItems animate__bounceInDown" to="/">
-          Home
-        </Link>
-        <Link className="navItems" to="/counter">
-          Counter
-        </Link>
-        <Link className="navItems" to="/about">
-          About US
-        </Link>
-      </div>
-    </header>
+    <div className="navWrapper">
+      <header className="nav">
+        <img src="/favicon.png" alt="Task Manger Logo" />
+        <div className="navContainer">
+          <Link className="navItems" to="/">
+            Home
+          </Link>
+          <Link className="navItems" to="/counter">
+            Counter
+          </Link>
+          <Link className="navItems" to="/about">
+            About US
+          </Link>
+        </div>
+      </header>
+    </div>
   );
 }
 
--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import TaskCard from "../components/TaskCard";
+import TaskCard from "../components/TaskCard.jsx";
 
 function Dashboard() {
   let tasks = [
